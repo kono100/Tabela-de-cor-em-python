7320ef7baf654dba52d13d79c9fef36101e55fe3
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-04-02 21:24:57 -0300

    Add files via upload

---
+++ Tabela de cor em python.py
@@ -0,0 +1,28 @@
+
+import colorama
+from colorama import Fore
+from colorama import Style
+
+colorama.init()
+print(Fore.BLUE + Style.BRIGHT + "This is the color of the sky" + Style.RESET_ALL)
+print(Fore.GREEN + "This is the color of grass" + Style.RESET_ALL)
+print(Fore.BLUE + Style.DIM + "This is a dimmer version of the sky" + Style.RESET_ALL)
+print(Fore.YELLOW + "This is the color of the sun" + Style.RESET_ALL)
+print(Fore.RED + "This is the color do sangue" + Style.RESET_ALL)
+print(2*"\n")
+
+
+#TABELA DE COR
+
+def print_format_table():
+    
+    for style in range(8):
+        for fg in range(30,38):
+            s1 = ''
+            for bg in range(40,48):
+                format = ';'.join([str(style), str(fg), str(bg)])
+                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
+            print(s1)
+        print('\n')
+
+print_format_table()
